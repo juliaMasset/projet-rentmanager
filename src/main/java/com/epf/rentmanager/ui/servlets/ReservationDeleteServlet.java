6905b418e4a4b960f98e3b8a473b9540c6b25da3
juliaMasset <100589004+juliaMasset@users.noreply.github.com>
2022-03-10 17:49:00 +0100

    delete resa terminé

--- src/main/java/com/epf/rentmanager/ui/servlets/ReservationDeleteServlet.java
+++ src/main/java/com/epf/rentmanager/ui/servlets/ReservationDeleteServlet.java
@@ -8,48 +8,40 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.context.support.SpringBeanAutowiringSupport;
-
 import com.epf.rentmanager.exception.ServiceException;
 import com.epf.rentmanager.model.Reservation;
 import com.epf.rentmanager.service.ReservationService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
+
 @WebServlet("/rents/delete")
-
 public class ReservationDeleteServlet extends HttpServlet {
+	
+    @Autowired
+    private ReservationService reservationService;
 
-	@Autowired
-	private ReservationService reservationService;
 
-	@Override
-	public void init() throws ServletException {
-		super.init();
-		SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
-	}
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
+    }
 
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            Reservation reservation = new Reservation(Integer.parseInt(request.getParameter("id")));
+            reservationService.delete(reservation);
+        } catch (ServiceException e) {
+            e.printStackTrace();
+        }
+        response.sendRedirect("/rentmanager/rents"); 
+    }
 
-		Reservation reservation = new Reservation(Integer.parseInt(request.getParameter("id")));
-
-		try {
-
-			reservationService.delete(reservation);
-
-		} catch (ServiceException e) {
-
-			e.printStackTrace();
-		}
-
-		response.sendRedirect("/rentmanager/rents");
-	}
-
-	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		doGet(request, response);
-	}
-
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
 }
